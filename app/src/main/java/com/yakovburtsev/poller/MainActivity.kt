6fb92568a26f0840cc927da49a003659f1b43cda
yakov <yakovburtsev@gmail.com>
2020-02-16 11:28:38 +0300

    Without BootReceiver

--- app/src/main/java/com/yakovburtsev/poller/MainActivity.kt
+++ app/src/main/java/com/yakovburtsev/poller/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.yakovburtsev.poller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val workRequest = PeriodicWorkRequest
+            .Builder(PollWorker::class.java, 10L, TimeUnit.SECONDS)
+            .build()
+
+        WorkManager.getInstance()
+            .enqueue(workRequest)
     }
 }
--- app/src/main/java/com/yakovburtsev/poller/PollWorker.kt
+++ app/src/main/java/com/yakovburtsev/poller/PollWorker.kt
@@ -4,11 +4,8 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import org.apache.http.HttpResponse
-import org.apache.http.StatusLine
-import org.apache.http.client.HttpClient
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 private const val TAG = "PollWorker"
@@ -16,12 +13,15 @@ private const val TAG = "PollWorker"
 class PollWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
 
     override fun doWork(): Result {
-        val httpclient: HttpClient = HttpClients.createDefault()
-        val response: HttpResponse = httpclient.execute(
-            HttpGet("http://finotifier.herokuapp.com/")
-        )
-        val statusLine: StatusLine = response.statusLine
-        Log.i(TAG, "Polled status: ${statusLine.statusCode}")
+        val urlString = "https://finotifier.herokuapp.com/"
+        val url = URL(urlString)
+        var urlConnection: HttpURLConnection? = null
+        try {
+            urlConnection = url.openConnection() as HttpURLConnection?
+            Log.i(TAG, "Polled url: $urlString, status: ${urlConnection?.responseCode}")
+        } finally {
+            urlConnection?.disconnect()
+        }
         return Result.success()
     }
 }
